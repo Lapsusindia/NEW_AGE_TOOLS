a9f31e87dbe75c20bd62b6ae395e4fecfaa2b5e3
Lapsusindia <164467341+Lapsusindia@users.noreply.github.com>
2024-04-15 22:30:03 +0530

    Update asciiart.py

--- cmd/asciiart.py
+++ cmd/asciiart.py
@@ -1,5 +1,9 @@
-  NNN   EEEEE  W   W    AAA   GGGGG  EEEEE   TTTTT  OOO  OOO  L      SSS
-  N NN  E      W   W   A   A G      E         T   O   O O   O L     S
-  N  NN EEE    W W W  AAAAA G  GG  EEE       T   O   O O   O L      SSS
-  N   N E       W W   A   A G   G  E         T   O   O O   O L         S
-  N   N EEEEE    W W   A   A  GGGG  EEEEE     T    OOO  OOO  LLLLL SSSS
+ascii_art = ('''
+ ________  ___ _   _ 
+/  ___|  \/  || | | |
+\ `--.| .  . || |_| |
+ `--. \ |\/| ||  _  |
+/\__/ / |  | || | | |
+\____/\_|  |_/\_| |_/
+                       
+  ''')
